e4c6de906f4668f7756ba3cc03743c039a1d8a59
bouhouchhamza <bouhouchhamza52@gmail.com>
2026-03-29 22:23:04 +0100

    feat: add model Simulation.php

---
+++ app/Models/Simulation.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Termwind\Components\Raw;
+
+class Simulation extends Model
+{
+    protected $fillable = [
+        'startup_id',
+        'duration',
+        'total_revenue',
+        'total_expenses',
+        'final_cashflow',
+        'risk_level',
+    ];
+    protected  $casts = [
+        'total_revenue' => 'decimal:2',
+        'total_expenses' => 'decimal:2',
+        'final_cashflow' => 'decimal:2'
+    ];
+
+
+    public function startup()
+    {
+        return $this->belongsTo(Startup::class);
+    }
+    public function simulationResults()
+    {
+        return $this->hasMany(SimulationResult::class);
+    }
+    public function reports()
+    {
+        return $this->hasMany(Report::class);
+    }
+}
